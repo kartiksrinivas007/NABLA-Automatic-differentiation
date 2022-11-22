008948ea586f352027ee08ecceda5ef9a875a826
Ganesh-RB <ganeshbombatkar777@gmail.com>
2022-11-22 22:33:29 +0530

    FIxed bug

--- Transpiler/ast.cpp
+++ Transpiler/ast.cpp
@@ -563,11 +563,11 @@ void Decl::transpile(std::ostream &out, int tab) const
     }
 
     out << "(";
-    out << "\"" << this->InitDeclaratorList->declarator->name << "\"";
+    out << "\"" << this->InitDeclaratorList->declarator->name << "\" ";
 
-    // if(this->InitDeclaratorList->declarator != nullptr || this->InitDeclaratorList->initializer!=nullptr){
-    //     out << " ,";
-    // }
+    if(!this->InitDeclaratorList->declarator->Dimensions.empty() || this->InitDeclaratorList->initializer != nullptr)
+        out << ", ";
+
     this->InitDeclaratorList->transpile(out, tab);
 
     out << ");" << std::endl;
@@ -588,6 +588,7 @@ void InitDeclarator::transpile(std::ostream &out, int tab) const
         }   
         else
         {
+            out << ", ";
             this->initializer->transpile(out, tab);
         }
     }
